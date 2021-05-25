2b83d927fa036a2ceeb19fcd4f8f6ebf3f1d53e2
Yarsolav Voroshilov <yrslv.voroshilov@gmail.com>
2021-05-25 20:00:40 +0300

    add example

---
+++ example/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
+const server = http.createServer(function(request, response) {
+	if (request.url === "/") {
+		fs.readFile('./EC.html', function (err, html) {
+			if (err) {
+				throw err; 
+			}  
+			response.writeHeader(200, {"Content-Type": "text/html"});  
+			response.write(html);  
+			response.end(); 
+		});
+	} else if (request.url.match(/.*\.(js|css|wasm)/)) {
+		fs.readFile('./' + lastPathSegment(request.url), function (err, file) {
+			if (err) {
+				throw err; 
+			}
+			response.write(file);  
+			response.end(); 
+		});
+	}
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function lastPathSegment(path) {
+	return path.substring(path.lastIndexOf('/') + 1)
+}
